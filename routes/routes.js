071f01b3269aa594e0d4b8ea83c4ce36f425366c
franko1307 <franciscogonzalez1307@gmail.com>
2017-01-27 22:16:20 -0700

    hey, what are you doing?

--- routes/routes.js
+++ routes/routes.js
@@ -2,4 +2,7 @@ module.exports = function (app) {
   app.get('/', function(req, res) {
 		res.render('index');
 	});
+  app.get('*', function(req, res) {
+		res.send('Hey, what are you doing?');
+	});
 }
