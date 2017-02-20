577f3c01c25ab551106c53497afd2f026f25e343
franko1307 <franciscogonzalez1307@gmail.com>
2017-02-20 00:01:26 -0700

    stuff

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,19 @@
 module.exports = function (app) {
-  app.get('/', function(req, res) {
-		res.render('index');
-	});
-  app.get('/ga-n-queen-problem', function(req, res) {
-		res.render('genetic-algorithm/n-queen-problem');
-	});
-  app.get('*', function(req, res) {
-		res.send('Hey, what are you doing?');
+
+    app.get('/', function(req, res) {
+		  res.render('index');
+	  });
+    app.get('/ga-n-queen-problem', function(req, res) {
+		    res.render('genetic-algorithm/n-queen-problem');
+	  });
+    app.get('/stuff', function(req,res){
+        res.render('links')
+    })
+
+    app.get('/ada-sort-algorithms',function(req,res){
+        res.render('ada/s_algorithms');
+    });
+    app.get('*', function(req, res) {
+		    res.send('Hey, what are you doing?');
 	});
 }
